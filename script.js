55752f24366b64b779286b3d04764baad14e8628
Anastasiia Shlapak <anastasiashlapak@icloud.com>
2022-11-08 12:10:21 +0100

    complete homework

---
+++ script.js
@@ -0,0 +1,15 @@
+let number1 = 45;
+let number2 = 12;
+console.log(number1 + number2);
+console.log(number1 - number2);
+console.log(number1 * number2);
+console.log(number1 / number2);
+
+let number = 456;
+number += 6;
+number++;
+number /= 2;
+number -= 3;
+number--;
+number %= 4;
+number += "qwerty";
